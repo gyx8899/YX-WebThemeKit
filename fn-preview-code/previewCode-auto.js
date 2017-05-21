102c55bbc63f3b3452e93b8cb82a01c03c7bf737
Foresky <gyx8899@126.com>
2017-05-21 14:24:41 +0800

    [Mod] Add previewCode auto js file;

---
+++ fn-preview-code/previewCode-auto.js
@@ -0,0 +1,5 @@
+loadScript('https://gyx8899.github.io/YX-WebThemeKit/fn-preview-code/previewCode.js', previewCodeCallback);
+function previewCodeCallback()
+{
+	previewAll($('#demoWrap'), $('#demoHTML'), $('#demoCSS'), $('#demoJS'));
+}
