e904fe06981dc738308c3a57142bc42b6463d59d
Foresky <gyx8899@126.com>
2017-05-21 14:54:49 +0800

    [Mod] Fix name of theme-header-footer;

--- theme-header-footer/header-footer-auto.js
+++ theme-header-footer/header-footer-auto.js
@@ -1,4 +1,4 @@
-loadScript('https://gyx8899.github.io/YX-WebThemeKit/theme-header-footer-owl/header-footer.js', headerFooterOwlCallback);
+loadScript('https://gyx8899.github.io/YX-WebThemeKit/theme-header-footer/header-footer.js', headerFooterOwlCallback);
 function headerFooterOwlCallback(type, options, config)
 {
 	type = type || 'owl';
