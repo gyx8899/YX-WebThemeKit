c8b4dc67efebc6824c125a2f8ab36b0280c0bde3
Foresky <gyx8899@126.com>
2017-05-23 21:19:06 +0800

    [Mod] Revert override default auto function;

--- fn-preview-code/previewCode-auto.js
+++ fn-preview-code/previewCode-auto.js
@@ -1,12 +1,9 @@
 loadScript('https://gyx8899.github.io/YX-WebThemeKit/fn-preview-code/previewCode.js', previewCodeCallback);
-if (typeof previewCodeCallback !== "function")
+function previewCodeCallback($codeParent, $demoHTML, $demoCSS, $demoJS, titleHTML, titleCSS, titleJS)
 {
-	function previewCodeCallback($codeParent, $demoHTML, $demoCSS, $demoJS, titleHTML, titleCSS, titleJS)
-	{
-		$codeParent = $codeParent || $('#demoWrap');
-		$demoHTML = $demoHTML || $('#demoHTML');
-		$demoCSS = $demoCSS || $('#demoCSS');
-		$demoJS = $demoJS || $('#demoJS');
-		previewAll($codeParent, $demoHTML, $demoCSS, $demoJS, titleHTML, titleCSS, titleJS);
-	}
+	$codeParent = $codeParent || $('#demoWrap');
+	$demoHTML = $demoHTML || $('#demoHTML');
+	$demoCSS = $demoCSS || $('#demoCSS');
+	$demoJS = $demoJS || $('#demoJS');
+	previewAll($codeParent, $demoHTML, $demoCSS, $demoJS, titleHTML, titleCSS, titleJS);
 }
--- theme-header-footer/header-footer-auto.js
+++ theme-header-footer/header-footer-auto.js
@@ -1,9 +1,6 @@
 loadScript('https://gyx8899.github.io/YX-WebThemeKit/theme-header-footer/header-footer.js', headerFooterOwlCallback);
-if (typeof headerFooterOwlCallback !== "function")
+function headerFooterOwlCallback(type, options, config)
 {
-	function headerFooterOwlCallback(type, options, config)
-	{
-		type = type || 'owl';
-		$.applyHeaderFooter(type, options, config);
-	}
+	type = type || 'owl';
+	$.applyHeaderFooter(type, options, config);
 }
