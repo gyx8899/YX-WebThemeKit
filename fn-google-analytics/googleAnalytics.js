c4f7b108fa80d014b92808ff3ca8e51c468be0e7
Foresky <gyx8899@126.com>
2017-08-09 19:25:54 +0800

    [Mod] Google Analytics: Remove the exist check of ga;
    Signed-off-by: yingxin.guo <yingxin.guo@neulionl.com.cn>

--- fn-google-analytics/googleAnalytics.js
+++ fn-google-analytics/googleAnalytics.js
@@ -6,14 +6,11 @@
 	}], currentSiteInfo = siteInfo.filter(function (site) {
 		return site.pathNameRoot === document.location.pathname.split('/')[1];
 	});
-	if (ga)
-	{
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-		ga('create', currentSiteInfo.trackID, 'auto');
-	}
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	ga('create', currentSiteInfo.trackID, 'auto');
 	if (document.location.pathname.indexOf(currentSiteInfo.pathNameRoot + '/') > -1) {
 	  var page = document.location.pathname.replace(currentSiteInfo.pathNameRoot + '/', '');
 	  ga('send', 'pageview', page);
