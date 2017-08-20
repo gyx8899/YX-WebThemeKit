2079d639b4d71947beca73b3995c892912554b07
Foresky <gyx8899@126.com>
2017-08-20 16:52:58 +0800

    [Mod] preLoader: loadResource with plugin;

--- fn-pre-loader/imageAnimate/preLoader-auto.js
+++ fn-pre-loader/imageAnimate/preLoader-auto.js
@@ -25,7 +25,7 @@
 
 		this._addPreLoader();
 
-		loadResources(this.options.resources);
+		LoadResources(this.options.resources);
 
 		this.options.endLoader && this._endPreLoaderOnLoaded();
 	};
@@ -170,11 +170,110 @@
 		}
 		return out;
 	}
+}());
 
-	/*
-	 * Functions: for loading resources
-	 * */
-	function loadCSS(url, callback, context)
+/**
+ * Javascript plugin: loadResources V1.3
+ * Support list:
+ * 1. css file;
+ * 2. js file;
+ *
+ * Params:
+ * 1. urls: single url string or url string array;
+ * 2. callback: execute when resource success;
+ *
+ * No support:
+ * 1. Not in IE when set callback and url string array;
+ *
+ * */
+(function () {
+	this.LoadResources = function (urls, callback) {
+		if (urls !== null && urls !== '')
+		{
+			if (Array.isArray(urls))
+			{
+				urls = urls.filter(function (url) {
+					return (String(url) === url && url !== '');
+				});
+				if (urls.length === 0)
+				{
+					callback && callback();
+				}
+				else if (urls.length === 1)
+				{
+					loadResource(urls[0], callback);
+				}
+				else
+				{
+					if (callback)
+					{
+						loadUrls(urls, callback);
+					}
+					else
+					{
+						var that = this;
+						urls.map(function (url) {
+							loadResource(url);
+						})
+					}
+				}
+			}
+			else if (String(urls) === urls)
+			{
+				loadResource(urls, callback);
+			}
+		}
+		else
+		{
+			callback && callback();
+		}
+	};
+
+	LoadResources.DEFAULTS = {
+		'js': {
+			name: 'js',
+			tagName: 'script',
+			urlAttrName: 'src',
+			loadFnName: 'loadJS',
+			loadFnPromiseName: 'loadJStWithPromise'
+		},
+		'css': {
+			name: 'css',
+			tagName: 'link',
+			urlAttrName: 'href',
+			loadFnName: 'loadCSS',
+			loadFnPromiseName: 'loadCSSWithPromise'
+		}
+	};
+
+	// Tools: functions
+	function loadResource(url)
+	{
+		if (!checkResourceLoaded(url))
+		{
+			eval(getUrlTypeInfo(url).loadFnName)(url);
+		}
+	}
+
+	function loadUrls(urls, callback)
+	{
+		var unLoadedResourcesInfo = urls.map(function (resource) {
+					var type = getUrlTypeInfo(resource),
+							resourceInfo = JSON.parse(JSON.stringify(LoadResources.DEFAULTS[type.name]));
+					resourceInfo.url = resource;
+					return resourceInfo;
+				}),
+				resourcePromise = unLoadedResourcesInfo.map(function (resourceInfo) {
+					return eval(resourceInfo.loadFnPromiseName)(resourceInfo.url);
+				});
+		Promise.all(resourcePromise).then(function () {
+			callback && callback();
+		}).catch(function (error) {
+			console.log("Error: in load resources! " + error);
+		});
+	}
+
+	function loadCSS(url, callback)
 	{
 		if (!url)
 			return;
@@ -192,7 +291,7 @@
 		document.getElementsByTagName('head')[0].appendChild(link);
 	}
 
-	function loadJS(url, callback, context)
+	function loadJS(url, callback)
 	{
 		var script = document.createElement("script");
 		script.type = "text/javascript";
@@ -218,11 +317,60 @@
 		document.body.appendChild(script);
 	}
 
-	// Functions: Parse and Process Url
-	var URL_TYPES = {
-			'js': {name: 'js', tagName: 'script', urlAttrName: 'src', loadFnName: 'loadJS', loadFnPromiseName: 'loadJSWithPromise'},
-			'css': {name: 'css', tagName: 'link', urlAttrName: 'href', loadFnName: 'loadCSS', loadFnPromiseName: 'loadJSWithPromise'}
-		};
+	function loadCSSWithPromise(url)
+	{
+		return new Promise(function (resolve, reject) {
+			if (!url)
+			{
+				reject(new Error("url is null!"));
+			}
+
+			var link = document.createElement('link');
+			link.rel = 'stylesheet';
+			link.type = 'text/css';
+			link.href = url;
+			link.onload = function () {
+				resolve();
+			};
+			link.onerror = function (error) {
+				reject(new Error(error));
+			};
+			document.getElementsByTagName('head')[0].appendChild(link);
+		});
+	}
+
+	function loadJStWithPromise(url)
+	{
+		return new Promise(function (resolve, reject) {
+			if (!url)
+			{
+				reject(new Error("url is null!"));
+			}
+
+			var script = document.createElement("script");
+			script.type = "text/javascript";
+
+			if (script.readyState)
+			{  //IE
+				script.onreadystatechange = function () {
+					if (script.readyState === "loaded" || script.readyState === "complete")
+					{
+						script.onreadystatechange = null;
+						resolve();
+					}
+				};
+			}
+			else
+			{  //Others
+				script.onload = function () {
+					resolve();
+				};
+			}
+
+			script.src = url;
+			document.body.appendChild(script);
+		});
+	}
 
 	function getFileNameFromURL(url)
 	{
@@ -247,44 +395,14 @@
 				resourceNameSplitArray = resourceName.split('.');
 		if (resourceNameSplitArray.length > 1)
 		{
-			return URL_TYPES[resourceNameSplitArray[resourceNameSplitArray.length - 1]];
+			return LoadResources.DEFAULTS[resourceNameSplitArray[resourceNameSplitArray.length - 1]];
 		}
 		return null;
 	}
-
-	function loadResource(url)
-	{
-		if (!checkResourceLoaded(url))
-		{
-			eval(getUrlTypeInfo(url).loadFnName)(url);
-		}
-	}
-
-	function loadResources(urls)
-	{
-		if (urls !== null && urls !== '')
-		{
-			if (Array.isArray(urls))
-			{
-				for (var i = 0, l = urls.length; i < l; i++)
-				{
-					loadResource(urls[i]);
-				}
-			}
-			else
-			{
-				loadResource(urls);
-			}
-		}
-		else
-		{
-			return null;
-		}
-	}
-}());
+})();
 
 // Instance PreLoader immediately
-var preLoader = new PreLoader({
+new PreLoader({
 	imageAnimate: true,
 	imageAnimateInfo: {
 		src: 'loading.svg'
