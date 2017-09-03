0238ff5c2776f939bf6cde899edd68fc353af989
Foresky <gyx8899@126.com>
2017-09-03 15:41:30 +0800

    [Mod] preLoader: Re-add loadResource function to plugin;

--- fn-pre-loader/preLoader.js
+++ fn-pre-loader/preLoader.js
@@ -1,9 +1,5 @@
 /**
- * Javascript plugin: PreLoader v1.6
- *
- * Dependency:
- * 1. common.js, for loading resource file (https://gyx8899.github.io/YX-JS-ToolKit/asset/js/common.js)
- *
+ * Javascript plugin: PreLoader v1.7
  */
 ;(function () {
 	this.PreLoader = function (options) {
@@ -174,3 +170,249 @@
 		return out;
 	}
 }());
+
+// Functions: loadResource
+// Copy from common.js (https://gyx8899.github.io/YX-JS-ToolKit/asset/js/common.js)
+(function () {
+	function loadResource(url, callback)
+	{
+		if (!checkResourceLoaded(url))
+		{
+			window[getUrlTypeInfo(url).loadFn](url, callback);
+		}
+	}
+
+	function loadResources(urls, callback)
+	{
+		if (urls !== null && urls !== '')
+		{
+			if (Array.isArray(urls))
+			{
+				urls = urls.filter(function (url) {
+					return (String(url) === url && url !== '');
+				});
+				if (urls.length === 0)
+				{
+					callback && callback();
+				}
+				else if (urls.length === 1)
+				{
+					loadResource(urls[0], callback);
+				}
+				else
+				{
+					if (callback)
+					{
+						loadUrls(urls, callback);
+					}
+					else
+					{
+						urls.map(function (url) {
+							loadResource(url);
+						})
+					}
+				}
+			}
+			else if (String(urls) === urls)
+			{
+				loadResource(urls, callback);
+			}
+		}
+		else
+		{
+			callback && callback();
+		}
+	}
+
+	function loadUrls(urls, callback)
+	{
+		var unLoadedResourcesInfo = urls.map(function (resource) {
+			var resourceInfo = getUrlTypeInfo(resource);
+			resourceInfo.url = resource;
+			return resourceInfo;
+		});
+		// If support Promise, use Promise
+		if (typeof Promise !== "undefined" && Promise.toString().indexOf("[native code]") !== -1)
+		{
+			var resourcePromise = unLoadedResourcesInfo.map(function (resourceInfo) {
+				return window[resourceInfo.loadFnPromise](resourceInfo.url);
+			});
+			Promise.all(resourcePromise).then(function () {
+				callback && callback();
+			}).catch(function (error) {
+				console.log("Error: in load resources! " + error);
+			});
+		}
+		else
+		{
+			unLoadedResourcesInfo.forEach(function (resourceInfo) {
+				window[resourceInfo.loadFn](resourceInfo.url);
+			});
+			callback && callback();
+		}
+	}
+
+	function loadCSS(url, callback, context)
+	{
+		if (!url)
+			return;
+
+		if (Array.isArray(url))
+		{
+			// Process the url and callback if they are array;
+			parameterArrayToItem(arguments.callee, url, callback);
+		}
+		else
+		{
+			var link = document.createElement('link');
+			link.rel = 'stylesheet';
+			link.type = 'text/css';
+			link.href = url;
+			link.onload = function () {
+				callback && (context ? context[callback]() : callback());
+			};
+			link.onerror = function () {
+				console.log("Error load css:" + url);
+			};
+			document.getElementsByTagName('head')[0].appendChild(link);
+		}
+	}
+
+	function loadScript(url, callback, context)
+	{
+		if (!url)
+			return;
+
+		if (Array.isArray(url))
+		{
+			// Process the url and callback if they are array;
+			parameterArrayToItem(arguments.callee, url, callback);
+		}
+		else
+		{
+			var script = document.createElement("script");
+			script.type = "text/javascript";
+
+			if (script.readyState)
+			{  //IE
+				script.onreadystatechange = function () {
+					if (script.readyState === "loaded" || script.readyState === "complete")
+					{
+						script.onreadystatechange = null;
+						callback && (context ? context[callback]() : callback());
+					}
+				};
+			}
+			else
+			{  //Others
+				script.onload = function () {
+					callback && (context ? context[callback]() : callback());
+				};
+			}
+
+			script.src = url;
+			document.body.appendChild(script);
+		}
+	}
+
+	// loadCSS with Promise
+	function loadCSSWithPromise(url)
+	{
+		return new Promise(function (resolve, reject) {
+			if (!url)
+			{
+				reject(new Error("url is null!"));
+			}
+
+			var link = document.createElement('link');
+			link.rel = 'stylesheet';
+			link.type = 'text/css';
+			link.href = url;
+			link.onload = function () {
+				resolve();
+			};
+			link.onerror = function (error) {
+				reject(new Error(error));
+			};
+			document.getElementsByTagName('head')[0].appendChild(link);
+		});
+	}
+
+	// loadScript with Promise
+	function loadScriptWithPromise(url)
+	{
+		return new Promise(function (resolve, reject) {
+			if (!url)
+			{
+				reject(new Error("url is null!"));
+			}
+
+			var script = document.createElement("script");
+			script.type = "text/javascript";
+
+			if (script.readyState)
+			{  //IE
+				script.onreadystatechange = function () {
+					if (script.readyState === "loaded" || script.readyState === "complete")
+					{
+						script.onreadystatechange = null;
+						resolve();
+					}
+				};
+			}
+			else
+			{  //Others
+				script.onload = function () {
+					resolve();
+				};
+			}
+
+			script.src = url;
+			document.body.appendChild(script);
+		});
+	}
+
+	function getFileNameFromURL(url)
+	{
+		return url.split('/').pop().split('#')[0].split('?')[0];
+	}
+
+	function checkResourceLoaded(url)
+	{
+		var type = getUrlTypeInfo(url),
+				typeSelector = type['tagName'] || '[src]',
+				allUrls = Array.prototype.slice.call(document.querySelectorAll(typeSelector))
+						.map(function (scriptElement) {
+							return scriptElement[type['urlAttrName']];
+						});
+		return allUrls.indexOf(url) !== -1;
+	}
+
+	function getUrlTypeInfo(url)
+	{
+		// Current only support js and css resources;
+		var resourceName = getFileNameFromURL(url),
+				resourceNameSplitArray = resourceName.split('.');
+		if (resourceNameSplitArray.length > 1)
+		{
+			var urlType = {
+				'js': {
+					name: 'js',
+					tagName: 'script',
+					urlAttrName: 'src',
+					loadFn: loadScript.name,
+					loadFnPromise: loadScriptWithPromise.name
+				},
+				'css': {
+					name: 'css',
+					tagName: 'link',
+					urlAttrName: 'href',
+					loadFn: loadCSS.name,
+					loadFnPromise: loadCSSWithPromise.name
+				}
+			};
+			return urlType[resourceNameSplitArray.pop()];
+		}
+		return null;
+	}
+})();
