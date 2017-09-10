bb272263a18547d3a6ec0dd6e3a6d5b0110c9d49
Foresky <gyx8899@126.com>
2017-09-10 10:39:14 +0800

    [Mod] GoogleAnalytics: update page with location.href when current page is not in SiteInfo;

--- fn-google-analytics/googleAnalytics.js
+++ fn-google-analytics/googleAnalytics.js
@@ -7,20 +7,23 @@
 		name: 'YX-WebThemeKit',
 		pathNameRoot: 'YX-WebThemeKit',
 		trackID: 'UA-104315567-2'
-	}], currentSiteInfo = siteInfo.filter(function (site) {
+	}],
+		currentSiteInfo = siteInfo.filter(function (site) {
 		return site.pathNameRoot === document.location.pathname.split('/')[1];
-	})[0];
+	})[0],
+		pathName = document.location.pathname,
+		page = document.location.href;
+	if (pathName.indexOf(currentSiteInfo.pathNameRoot.toLowerCase() + '/') > -1)
+	{
+		page = pathName.replace('/' + currentSiteInfo.pathNameRoot + '/', '')
+				.replace('/index.html', '')
+				.replace('.html', '');
+		page = page || currentSiteInfo.name; // set default page name
+	}
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 	ga('create', currentSiteInfo.trackID, 'auto');
-	var pathName = document.location.pathname;
-	if (pathName.indexOf(currentSiteInfo.pathNameRoot + '/') > -1) {
-	  var page = pathName.replace('/' + currentSiteInfo.pathNameRoot + '/', '')
-			  .replace('/index.html', '')
-			  .replace('.html', '');
-	  page = page || currentSiteInfo.name; // set default page name
-	  ga('send', 'pageview', page);
-	}
+	ga('send', 'pageview', page);
 })();
