e9508f37273122e80c4f5711eafc370bfb941b7e
Foresky <gyx8899@126.com>
2017-11-14 18:44:13 +0800

    [Add] funDebug : Set default others config;

--- theme-header-footer/headerFooter-auto.js
+++ theme-header-footer/headerFooter-auto.js
@@ -11,11 +11,15 @@
 				name: 'YX-CSS-ToolKit',
 				pathNameRoot: 'YX-CSS-ToolKit',
 				theme: 'https://gyx8899.github.io/YX-WebThemeKit/theme-header-footer/owl/headerFooter-auto.js'
+			}, {
+				name: 'Others',
+				pathNameRoot: '',
+				theme: 'https://gyx8899.github.io/YX-WebThemeKit/theme-header-footer/owl/headerFooter-auto.js'
 			}],
 			pathName = document.location.pathname,
 			currentSiteInfo = siteInfo.filter(function (site) {
 				return site.pathNameRoot.toLowerCase() === pathName.split('/')[1].toLowerCase();
-			})[0];
+			})[0] || siteInfo[siteInfo.length - 1];
 
 	loadScript(currentSiteInfo.theme);
 
