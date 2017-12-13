770269ec459af6529a9190390e2f58c19151a680
Foresky <gyx8899@126.com>
2017-12-13 23:35:09 +0800

    [Mod] funDebug : Reset with the config change;

--- fn-fun-debug/funDebug.js
+++ fn-fun-debug/funDebug.js
@@ -1,8 +1,8 @@
+(siteConfig && siteConfig.config.funDebug) &&
 (function () {
 	var funDebugLibUrl = "https://og6593g2z.qnssl.com/fundebug.0.3.3.min.js",
-			siteInfo = [{
+			funDebugConfig = [{
 				name: 'YX-JS-ToolKit',
-				pathNameRoot: 'YX-JS-ToolKit',
 				apikey: 'f3b60739271056d85641a316cd13350f5960922b510427e3ec514bc3f0a74ac5',
 				appversion: '',
 				releaseStage: '', // 'development', 'test', 'production'
@@ -11,7 +11,6 @@
 				silentHttp: false
 			}, {
 				name: 'YX-WebThemeKit',
-				pathNameRoot: 'YX-WebThemeKit',
 				apikey: 'af39dba572be6c381017954d7728bd8282cb5d56ee452fb31127ea8458a5a016',
 				appversion: '',
 				releaseStage: '', // 'development', 'test', 'production'
@@ -20,7 +19,6 @@
 				silentHttp: false
 			}, {
 				name: 'YX-CSS-ToolKit',
-				pathNameRoot: 'YX-CSS-ToolKit',
 				apikey: '3aa41ba4e15588c7c9d11bc4f227d38165095a36ee1619ac0cb06ac163005376',
 				appversion: '',
 				releaseStage: '', // 'development', 'test', 'production'
@@ -29,7 +27,6 @@
 				silentHttp: false
 			}, {
 				name: 'Others',
-				pathNameRoot: '',
 				apikey: '77900ef947409eaa50cf1ad42697c12eadd3600dbd0a8d4c112eef36fca21fee',
 				appversion: '',
 				releaseStage: '', // 'development', 'test', 'production'
@@ -37,12 +34,12 @@
 				silentResource: false,
 				silentHttp: false
 			}],
-			pathName = document.location.pathname,
-			currentSiteInfo = siteInfo.filter(function (site) {
-				return site.pathNameRoot.toLowerCase() === pathName.split('/')[1].toLowerCase();
-			})[0] || siteInfo[siteInfo.length - 1];
 
-	loadScript(funDebugLibUrl, currentSiteInfo);
+			siteFunDebugConfig = funDebugConfig.filter(function (site) {
+				return site.name.toLowerCase() === siteConfig.name.toLowerCase();
+			})[0];
+
+	loadScript(funDebugLibUrl, siteFunDebugConfig);
 
 	function loadScript(url, siteInfo)
 	{
