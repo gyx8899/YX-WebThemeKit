41d9f991330c82ba6ee25b4da54a37950af1fe0f
Foresky <gyx8899@126.com>
2017-12-13 23:58:28 +0800

    [Mod] Google Analytics : Update with config change;

--- fn-google-analytics/googleAnalytics.js
+++ fn-google-analytics/googleAnalytics.js
@@ -1,38 +1,36 @@
+(siteConfig && siteConfig.config.googleAnalytics) &&
 (function () {
-	var siteInfo = [{
+	var googleAnalyticsConfig = [{
 				name: 'YX-JS-ToolKit',
-				pathNameRoot: 'YX-JS-ToolKit',
 				trackID: 'UA-104315567-1'
 			}, {
 				name: 'YX-WebThemeKit',
-				pathNameRoot: 'YX-WebThemeKit',
 				trackID: 'UA-104315567-2'
 			}, {
 				name: 'YX-CSS-ToolKit',
-				pathNameRoot: 'YX-CSS-ToolKit',
 				trackID: 'UA-104315567-3'
 			}],
-		pathName = document.location.pathname,
-		currentSiteInfo = siteInfo.filter(function (site) {
-			return site.pathNameRoot.toLowerCase() === pathName.split('/')[1].toLowerCase();
-		})[0],
-		page = document.location.href;
-	if (currentSiteInfo)
-	{
-		page = pathName.replace('/' + pathName.split('/')[1] + '/', '')
-				.replace('/index.html', '')
-				.replace('index.html', '')
-				.replace('.html', '')
-				|| currentSiteInfo.name; // Default page name
-	}
-	else
-	{
-		currentSiteInfo = siteInfo[0];  // Default YX-JS-ToolKit
-	}
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	ga('create', currentSiteInfo.trackID, 'auto');
+			siteGoogleAnalyticsConfig = googleAnalyticsConfig.filter(function (site) {
+				return site.name.toLowerCase() === siteConfig.name.toLowerCase();
+			})[0],
+			pathName = document.location.pathname,
+			page = pathName.replace('/' + pathName.split('/')[1] + '/', '')
+							.replace('/index.html', '')
+							.replace('index.html', '')
+							.replace('.html', '')
+					|| siteGoogleAnalyticsConfig.name; // Default page name
+
+	(function (i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function () {
+			(i[r].q = i[r].q || []).push(arguments)
+		}, i[r].l = 1 * new Date();
+		a = s.createElement(o),
+				m = s.getElementsByTagName(o)[0];
+		a.async = 1;
+		a.src = g;
+		m.parentNode.insertBefore(a, m)
+	})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+	ga('create', siteGoogleAnalyticsConfig.trackID, 'auto');
 	ga('send', 'pageview', page);
 })();
