9a3122ad737fab0f9a643a12f2ef7680cead5d7b
Foresky <gyx8899@126.com>
2018-01-07 15:26:38 +0800

    [Mod] githubRibbon : Optimize code and use common function;

--- theme-github-ribbon/githubRibbon.js
+++ theme-github-ribbon/githubRibbon.js
@@ -18,97 +18,130 @@
 				return site.name.toLowerCase() === siteConfig.name.toLowerCase();
 			})[0],
 
-			imageType = {
-				leftRed: {
-					position: 'left',
-					src: "https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png"
-				},
-				leftGreen: {
-					position: 'left',
-					src: "https://camo.githubusercontent.com/121cd7cbdc3e4855075ea8b558508b91ac463ac2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png"
-				},
-				leftBlack: {
-					position: 'left',
-					src: "https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png"
-				},
-				leftYellow: {
-					position: 'left',
-					src: "https://camo.githubusercontent.com/8b6b8ccc6da3aa5722903da7b58eb5ab1081adee/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6f72616e67655f6666373630302e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png"
-				},
-				leftGrey: {
-					position: 'left',
-					src: "https://camo.githubusercontent.com/c6625ac1f3ee0a12250227cf83ce904423abf351/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677261795f3664366436642e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png"
-				},
-				leftWhite: {
-					position: 'left',
-					src: "https://camo.githubusercontent.com/c6286ade715e9bea433b4705870de482a654f78a/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f77686974655f6666666666662e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_white_ffffff.png"
-				},
-				rightRed: {
-					position: 'right',
-					src: "https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
-				},
-				rightGreen: {
-					position: 'right',
-					src: "https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"
-				},
-				rightBlack: {
-					position: 'right',
-					src: "https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
-				},
-				rightYellow: {
-					position: 'right',
-					src: "https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"
-				},
-				rightGrey: {
-					position: 'right',
-					src: "https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"
-				},
-				rightWhite: {
-					position: 'right',
-					src: "https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67",
-					'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"
-				}
+			defaultImageAttr = {
+				id: 'githubRibbonId',
+				alt: 'Fork me on GitHub',
+				style: 'position: absolute; top: 0; border: 0; z-index: 100;'
 			},
 
+			imageType = {
+				leftRed: {
+					style: {
+						left: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png"
+					}
+				},
+				leftGreen: {
+					style: {
+						left: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/121cd7cbdc3e4855075ea8b558508b91ac463ac2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png"
+					}
+				},
+				leftBlack: {
+					style: {
+						left: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png"
+					}
+				},
+				leftYellow: {
+					style: {
+						left: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/8b6b8ccc6da3aa5722903da7b58eb5ab1081adee/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6f72616e67655f6666373630302e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png"
+					}
+				},
+				leftGrey: {
+					style: {
+						left: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/c6625ac1f3ee0a12250227cf83ce904423abf351/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677261795f3664366436642e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png"
+					}
+				},
+				leftWhite: {
+					style: {
+						left: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/c6286ade715e9bea433b4705870de482a654f78a/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f77686974655f6666666666662e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_left_white_ffffff.png"
+					}
+				},
+				rightRed: {
+					style: {
+						right: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
+					}
+				},
+				rightGreen: {
+					style: {
+						right: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"
+					}
+				},
+				rightBlack: {
+					style: {
+						right: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+					}
+				},
+				rightYellow: {
+					style: {
+						right: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"
+					}
+				},
+				rightGrey: {
+					style: {
+						right: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"
+					}
+				},
+				rightWhite: {
+					style: {
+						right: 0
+					},
+					attr: {
+						src: "https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67",
+						'data-canonical-src': "https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"
+					}
+				}
+			},
+			imageInfo = deepExtend(imageType[siteRibbonConfig.type], {attr: defaultImageAttr}),
 			githubRibbon = document.createElement('a');
+
 	githubRibbon.setAttribute('href', 'https://github.com/' + githubAuthor + '/' + siteRibbonConfig.repo);
-	githubRibbon.innerHTML = getRibbonImgHTML(imageType[siteRibbonConfig.type]);
+	githubRibbon.innerHTML = createTagElement('img', imageInfo).outerHTML;
+
 	document.body.appendChild(githubRibbon);
 
-	setMediaQueryStyle();
-
-	function getRibbonImgHTML(imageInfo)
-	{
-		var img = document.createElement('img');
-		img.id = "githubRibbonId";
-		img.setAttribute('style', 'position: absolute; top: 0; border: 0; z-index: 100;');
-		img.style[imageInfo.position] = 0;
-		img.setAttribute('alt', 'Fork me on GitHub');
-		img.setAttribute('src', imageInfo.src);
-		img.setAttribute('data-canonical-src', imageInfo['data-canonical-src']);
-		return img.outerHTML;
-	}
-
-	function setMediaQueryStyle()
-	{
-		var ribbonStyle = '@media (max-width: 767px)' +
-				'{' +
-				'#githubRibbonId' +
-				'{' +
-				'display: none;' +
-				'}' +
-				'}';
-		addStyleToHead(ribbonStyle);
-	}
+	// Hide github ribbon when window.width < 768px
+	insertStyleToHead('@media (max-width: 767px){#githubRibbonId{display: none;}}');
 })();
