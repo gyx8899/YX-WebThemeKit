35df7083d7e0d02c26360c80fcffd5c9569b81b0
Steper <gyx8899@126.com>
2018-04-01 11:59:22 +0800

    [Mod] googleAnalytics : update after setting uglifyJS;

--- fn-google-analytics/googleAnalytics.js
+++ fn-google-analytics/googleAnalytics.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function () {
 	var googleAnalyticsConfig = [{
 				name: 'YX-JS-ToolKit',
@@ -13,23 +15,20 @@
 				return site.name.toLowerCase() === siteConfig.name.toLowerCase();
 			})[0],
 			pathName = document.location.pathname,
-			page = pathName.replace('/' + pathName.split('/')[1] + '/', '')
-							.replace('/index.html', '')
-							.replace('index.html', '')
-							.replace('.html', '')
-					|| siteGoogleAnalyticsConfig.name; // Default page name
+			page = pathName.replace('/' + pathName.split('/')[1] + '/', '').replace('/index.html', '').replace('index.html', '').replace('.html', '') || siteGoogleAnalyticsConfig.name; // Default page name
 
 	(function (i, s, o, g, r, a, m) {
 		i['GoogleAnalyticsObject'] = r;
 		i[r] = i[r] || function () {
-			(i[r].q = i[r].q || []).push(arguments)
+			(i[r].q = i[r].q || []).push(arguments);
 		}, i[r].l = 1 * new Date();
-		a = s.createElement(o),
-				m = s.getElementsByTagName(o)[0];
+		a = s.createElement(o), m = s.getElementsByTagName(o)[0];
 		a.async = 1;
 		a.src = g;
-		m.parentNode.insertBefore(a, m)
+		m.parentNode.insertBefore(a, m);
 	})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 	ga('create', siteGoogleAnalyticsConfig.trackID, 'auto');
 	ga('send', 'pageview', page);
 })();
+
+//# sourceMappingURL=googleAnalytics.js.map
