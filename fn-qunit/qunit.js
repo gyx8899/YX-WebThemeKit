5f53a75dac7953fef4f52aeef6923674a5961e27
Steper <gyx8899@126.com>
2018-04-05 00:07:30 +0800

    [Mod] Qunit : Update to add comment info and support YX;

--- fn-qunit/qunit.js
+++ fn-qunit/qunit.js
@@ -1,14 +1,17 @@
 'use strict';
 
-(function () {
+/**
+ * Qunit integrate plugin v2.0.0.180404_beta
+ */
+(function (YX) {
 	var qUnitFolder = document.createElement('div');
 	qUnitFolder.innerHTML = '<div id="qunit"></div><div id="qunit-fixture"></div>';
 	document.body.insertBefore(qUnitFolder, document.body.firstChild);
 
-	loadCSS('https://code.jquery.com/qunit/qunit-2.4.1.css');
-	loadScript('https://code.jquery.com/qunit/qunit-2.4.1.js', function () {
-		loadScript(getRootPath() + '/test.js');
+	YX.Util.load.loadCSS('https://code.jquery.com/qunit/qunit-2.4.1.css');
+	YX.Util.load.loadScript('https://code.jquery.com/qunit/qunit-2.4.1.js', function () {
+		YX.Util.load.loadScript(getRootPath() + '/test.js');
 	});
-})();
+})(window.YX);
 
 //# sourceMappingURL=qunit.js.map
