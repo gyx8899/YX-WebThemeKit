c47493fc0fd006534415e44205de9389dbc289b9
Steper <gyx8899@126.com>
2018-05-16 22:42:17 +0800

    [Mod] Remove useless yx-theme-config files and update js with es6;

--- assets/js/yx-theme-config.js
+++ assets/js/yx-theme-config.js
@@ -1,23 +1,24 @@
-'use strict';
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 /**
  * Site Config v1.0.2.180508_beta
  */
 (function (root, factory) {
-	if (typeof define === 'function' && define.amd) {
+	if (typeof define === 'function' && define.amd)
+	{
 		define(['yx'], factory);
 		// define(['jquery', 'underscore'], factory);
-	} else if ((typeof module === 'undefined' ? 'undefined' : _typeof(module)) === 'object' && module.exports) {
+	}
+	else if (typeof module === 'object' && module.exports)
+	{
 		module.exports = factory(require('yx'));
 		// module.exports = factory(require('jquery'), require('underscore'));
-	} else {
+	}
+	else
+	{
 		root.siteConfig = factory(root.YX);
 		// root.SiteConfig = factory(root.jQuery, root._);
 	}
-})(window, function (YX) {
-	var DEFAULT_CONFIG = {
+}(window, function (YX) {
+	let DEFAULT_CONFIG = {
 				headerFooter: true,
 				googleAnalytics: true,
 				githubRibbon: true,
@@ -29,57 +30,52 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 			YX_SITE_CONFIG = [{
 				name: 'YX-JS-ToolKit',
 				pathNameRoot: 'YX-JS-ToolKit',
-				customConfig: {}
-	}, {
-		name: 'YX-WebThemeKit',
-		pathNameRoot: 'YX-WebThemeKit',
-				customConfig: {}
-	}, {
-		name: 'YX-CSS-ToolKit',
-		pathNameRoot: 'YX-CSS-ToolKit',
-				customConfig: {}
-	}, {
-		name: 'Others',
-		pathNameRoot: '',
-				customConfig: {}
-	}],
-	    configUrl = {
-		headerFooter: {
-			firstScreen: true,
-			url: 'https://gyx8899.github.io/YX-WebThemeKit/theme-header-footer/headerFooter.min.js'
-		},
-		googleAnalytics: {
-			url: 'https://gyx8899.github.io/YX-WebThemeKit/fn-google-analytics/googleAnalytics.min.js'
-		},
-		githubRibbon: {
-			url: 'https://gyx8899.github.io/YX-WebThemeKit/theme-github-ribbon/githubRibbon.min.js'
-		},
-		fixedToolbar: {
-			url: 'https://gyx8899.github.io/YX-WebThemeKit/theme-fixed-toolbar/fixedToolbar.min.js'
-		},
-		previewCode: {
-			url: 'https://gyx8899.github.io/YX-WebThemeKit/fn-preview-code/previewCode.min.js?init=auto',
-			condition: function condition() {
-				return document.querySelectorAll('[data-toggle="previewCode"]').length;
-			}
-		},
-		qUnit: {
-			url: 'https://gyx8899.github.io/YX-WebThemeKit/fn-qunit/qunit.min.js',
-			condition: function condition() {
-				return siteConfig.queryParams['qunit'] === 'true';
-			}
-		},
-		disqus: {
-			url: 'https://gyx8899.github.io/YX-WebThemeKit/fn-disqus/disqus.min.js',
-			condition: function condition() {
-				return !YX.Util.navigator.isZHLanguage();
-			}
-		}
-	},
-	    sitePathName = document.location.pathname,
-	    siteConfig = YX_SITE_CONFIG.filter(function (site) {
-		return site.pathNameRoot.toLowerCase() === sitePathName.split('/')[1].toLowerCase();
-	})[0] || YX_SITE_CONFIG[YX_SITE_CONFIG.length - 1];
+				customConfig: {},
+			}, {
+				name: 'YX-WebThemeKit',
+				pathNameRoot: 'YX-WebThemeKit',
+				customConfig: {},
+			}, {
+				name: 'YX-CSS-ToolKit',
+				pathNameRoot: 'YX-CSS-ToolKit',
+				customConfig: {},
+			}, {
+				name: 'Others',
+				pathNameRoot: '',
+				customConfig: {},
+			}],
+			configUrl = {
+				headerFooter: {
+					firstScreen: true,
+					url: 'https://gyx8899.github.io/YX-WebThemeKit/theme-header-footer/headerFooter.min.js'
+				},
+				googleAnalytics: {
+					url: 'https://gyx8899.github.io/YX-WebThemeKit/fn-google-analytics/googleAnalytics.min.js'
+				},
+				githubRibbon: {
+					url: 'https://gyx8899.github.io/YX-WebThemeKit/theme-github-ribbon/githubRibbon.min.js'
+				},
+				fixedToolbar: {
+					url: 'https://gyx8899.github.io/YX-WebThemeKit/theme-fixed-toolbar/fixedToolbar.min.js'
+				},
+				previewCode: {
+					url: 'https://gyx8899.github.io/YX-WebThemeKit/fn-preview-code/previewCode.min.js?init=auto',
+					condition: () => document.querySelectorAll('[data-toggle="previewCode"]').length
+				},
+				qUnit: {
+					url: 'https://gyx8899.github.io/YX-WebThemeKit/fn-qunit/qunit.min.js',
+					condition: () => siteConfig.queryParams['qunit'] === 'true'
+				},
+				disqus: {
+					url: 'https://gyx8899.github.io/YX-WebThemeKit/fn-disqus/disqus.min.js',
+					condition: () => !YX.Util.navigator.isZHLanguage()
+				}
+			},
+
+			sitePathName = document.location.pathname,
+			siteConfig = YX_SITE_CONFIG.filter(function (site) {
+				return site.pathNameRoot.toLowerCase() === sitePathName.split('/')[1].toLowerCase();
+			})[0] || YX_SITE_CONFIG[YX_SITE_CONFIG.length - 1];
 
 	siteConfig.queryParams = YX.Util.url.getUrlQueryParams();
 
@@ -87,81 +83,94 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 	enableServiceWorker();
 
-	window.addEventListener("load", function () {
-		return siteConfig && loadConfigs(siteConfig, false);
-	}, false);
+	window.addEventListener("load", () => siteConfig && loadConfigs(siteConfig, false), false);
 
 	siteConfig && loadConfigs(siteConfig, true);
 
 	/**
-  * Enable PWA server worker when it is available
-  */
-	function enableServiceWorker() {
-		if ('serviceWorker' in navigator) {
+	 * Enable PWA server worker when it is available
+	 */
+	function enableServiceWorker()
+	{
+		if ('serviceWorker' in navigator)
+		{
 			window.addEventListener('load', function () {
-				var pathNameRoot = siteConfig.pathNameRoot ? siteConfig.pathNameRoot + '/' : '',
+				let pathNameRoot = siteConfig.pathNameRoot ? siteConfig.pathNameRoot + '/' : '',
 						swPath = this.location.origin + '/' + pathNameRoot + 'service-worker.js',
 						swScope = '/' + pathNameRoot;
-				navigator.serviceWorker.register(swPath, {scope: swScope}).then(function (registration) {
-					// Registration successful
-					console.log('ServiceWorker registration successful with scope: ', registration.scope);
-				}).catch(function (err) {
-					// Registration failed
-					console.log('ServiceWorker registration failed: ', err);
-				});
+				navigator.serviceWorker.register(swPath, {scope: swScope})
+						.then(function (registration) {
+							// Registration successful
+							console.log('ServiceWorker registration successful with scope: ', registration.scope);
+						})
+						.catch(function (err) {
+							// Registration failed
+							console.log('ServiceWorker registration failed: ', err);
+						});
 			});
 		}
 	}
 
 	/***
-  * Load site config components
-  * @param {Object} configInfo
-  * @param {Boolean} isFirstScreen, true: load resource before dom ready, false: load resource after dom ready
-  */
-	function loadConfigs(configInfo, isFirstScreen) {
-		for (var config in DEFAULT_CONFIG)
+	 * Load site config components
+	 * @param {Object} configInfo
+	 * @param {Boolean} isFirstScreen, true: load resource before dom ready, false: load resource after dom ready
+	 */
+	function loadConfigs(configInfo, isFirstScreen)
+	{
+		for (let config in DEFAULT_CONFIG)
 		{
-			if ((configInfo.customConfig && configInfo.customConfig.hasOwnProperty(config) ? configInfo.customConfig[config] : DEFAULT_CONFIG[config]) && (isFirstScreen && configUrl[config].firstScreen || !isFirstScreen && !configUrl[config].firstScreen) && (!configUrl[config].condition || configUrl[config].condition()))
+			if (((configInfo.customConfig && configInfo.customConfig.hasOwnProperty(config)) ? configInfo.customConfig[config] : DEFAULT_CONFIG[config]) &&
+					((isFirstScreen && configUrl[config].firstScreen) || (!isFirstScreen && !configUrl[config].firstScreen)) &&
+					(!configUrl[config].condition || configUrl[config].condition()))
 			{
-				YX.Util.load.loadScript(configUrl[config].url, null, null, { isAsync: !isFirstScreen });
+				YX.Util.load.loadScript(configUrl[config].url, null, null, {isAsync: !isFirstScreen});
 			}
 		}
 	}
 
 	/**
-  * Load Dev resource when url has param '&env=dev'
-  */
-	function handleParameters() {
+	 * Load Dev resource when url has param '&env=dev'
+	 */
+	function handleParameters()
+	{
 		// Handle page parameters
-		if (siteConfig.name === 'YX-WebThemeKit' && (siteConfig.queryParams['env'] === 'dev' || siteConfig.queryParams['_ijt'] !== '')) {
+		if (siteConfig.name === 'YX-WebThemeKit' &&
+				(siteConfig.queryParams['env'] === 'dev' || siteConfig.queryParams['_ijt'] !== ''))
+		{
 			Object.keys(configUrl).forEach(function (key) {
 				configUrl[key].url = configUrl[key].url.replace('https://gyx8899.github.io/', '../../../');
 			});
 		}
 
 		// Handle theme config parameters
-		var themeConfigParams = YX.Util.url.getUrlQueryParams(getScriptName());
-		if (themeConfigParams['ignore']) {
-			themeConfigParams['ignore'].split(',').forEach(function (themeName) {
+		let themeConfigParams = YX.Util.url.getUrlQueryParams(getScriptName());
+		if (themeConfigParams['ignore'])
+		{
+			themeConfigParams['ignore'].split(',').forEach(themeName => {
 				configUrl[themeName].url = '';
-			});
+			})
 		}
 	}
 
 	/**
-  * getScriptName
-  * @return {*}
-  */
-	function getScriptName() {
-		var error = new Error(),
-		    source = void 0,
-		    lastStackFrameRegex = new RegExp(/.+\/(.*?):\d+(:\d+)*$/),
-		    currentStackFrameRegex = new RegExp(/getScriptName \(.+\/(.*):\d+:\d+\)/);
+	 * getScriptName
+	 * @return {*}
+	 */
+	function getScriptName()
+	{
+		let error = new Error()
+				, source
+				, lastStackFrameRegex = new RegExp(/.+\/(.*?):\d+(:\d+)*$/)
+				, currentStackFrameRegex = new RegExp(/getScriptName \(.+\/(.*):\d+:\d+\)/);
 
-		if (error.stack && (source = lastStackFrameRegex.exec(error.stack.trim())) && source.length > 1 && source[1] !== "") return source[1];else if (error.stack && (source = currentStackFrameRegex.exec(error.stack.trim()))) return source[1];else if (error['fileName'] !== undefined) return error['fileName'];
+		if (error.stack && (source = lastStackFrameRegex.exec(error.stack.trim())) && (source.length > 1 && source[1] !== ""))
+			return source[1];
+		else if (error.stack && (source = currentStackFrameRegex.exec(error.stack.trim())))
+			return source[1];
+		else if (error['fileName'] !== undefined)
+			return error['fileName'];
 	}
 
 	return siteConfig;
-});
-
-//# sourceMappingURL=yx-theme-config.js.map
+}));
